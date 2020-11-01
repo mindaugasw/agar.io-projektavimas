92430fd7def3b2c9649b2304786e81c45e6c2693
ImSpeakEnglish <mindaugas932@gmail.com>
2020-11-01 02:23:18 +0200

    Config manager

---
+++ agar-client/Game/ConfigManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
+
+namespace agar_client.Game
+{
+	class ConfigManager
+	{
+		static Dictionary<string, string> configDict;
+		static bool initialized = false;
+
+		/// <summary>
+		/// Get config value by key.
+		/// </summary>
+		public static T Get<T>(string key)
+		{
+			if (!initialized)
+				Initialize();
+
+			if (!configDict.ContainsKey(key))
+				throw new Exception("Key does not exist.");
+
+			return (T)Convert.ChangeType(configDict[key], typeof(T));
+		}
+
+		static void Initialize()
+		{
+			configDict = new Dictionary<string, string>();
+
+			var dir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+			var mainConfigFile = Path.Combine(dir, "config.xml");
+			var localConfigFile = Path.Combine(dir, "config.local.xml");
+
+			if (File.Exists(mainConfigFile))
+				ReadConfigFile(mainConfigFile);
+
+			if (File.Exists(localConfigFile))
+				ReadConfigFile(localConfigFile);
+			
+			initialized = true;
+		}
+
+		static void ReadConfigFile(string path)
+		{
+			XDocument doc = XDocument.Load(path);
+
+			foreach (var e in doc.Descendants("add"))
+				configDict[e.Attribute("key").Value] = e.Attribute("value").Value;
+		}
+	}
+}
